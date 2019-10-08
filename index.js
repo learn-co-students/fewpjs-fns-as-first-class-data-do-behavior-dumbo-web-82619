9c90eeb14d4ea49726584a443f4bc7633ac2ea26
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-10-08 12:45:42 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeArg) {
+  let timeArgString = timeArg.split(':')[0];
+  let timeHour = parseInt(timeArgString);
+
+  if (timeHour < 12) {
+    return 'Good Morning';
+  }
+  if (timeHour >= 12 && timeHour < 17) {
+    return 'Good Afternoon';
+  } 
+  if (timeHour > 17) {
+    return 'Good Evening';
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(greet) {
+  let greeting = document.querySelector('#greeting')
+  greeting.innerText = greet
+}
